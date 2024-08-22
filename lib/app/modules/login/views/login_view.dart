4f72a7721b288a97e60a791ecb2048fa49be617b
salmanananda2 <salmanananda2@gmail.com>
2024-08-22 00:34:43 +0000

    first commit

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller.emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: controller.passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            Obx(() => controller.isLoading.value
+              ? CircularProgressIndicator()
+              : ElevatedButton(
+                onPressed: controller.login,
+                child: Text('Login'),
+              )),
+            TextButton(onPressed: () {
+              Get.toNamed('/register');
+            },
+             child: Text('Dont have an account? Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
